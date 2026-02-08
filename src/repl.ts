36ca60d8575b05c53665bdd26be0d194ecc5df2c
mohmamad <mohamad.isa.moghrabi@gmail.com>
2026-02-09 01:50:28 +0200

    Add repl interface and logic using node readline

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,5 @@
+import readline from "readline";
+
 export function cleanInput(input: string): string[] {
   const cleanedText = input.trim().toLowerCase().replace(/\s+/g," ").split(" ").filter(Boolean);
   for(const text in cleanedText){
@@ -6,4 +8,29 @@ export function cleanInput(input: string): string[] {
     }
   }
   return cleanedText;
+}
+
+export function startREPL(){
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.setPrompt("Pokedex > ");
+
+    rl.prompt();
+
+    rl.on("line" , (input: string) => {
+      const words = cleanInput(input);
+
+      if(words.length === 0) console.log("Your command was: ");
+      else console.log(`Your command was: ${words[0]}`);
+
+      rl.prompt();
+    })
+
+    rl.on("close", () => {
+      process.exit(0);
+    });
+
 }
