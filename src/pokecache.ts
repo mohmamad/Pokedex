e74fdb1676731fa9504d02a05715656a705115b2
mohmamad <mohamad.isa.moghrabi@gmail.com>
2026-02-10 06:13:31 +0200

    Add in-memory cache for pokeAPI responses

---
+++ src/pokecache.ts
@@ -0,0 +1,44 @@
+
+
+export type CacheEntry<T> = {
+    createdAt: number;
+    val : T;
+}
+
+export class Cache {
+  #cache = new Map<string, CacheEntry<any>>();
+  #reapIntervalId: NodeJS.Timeout|undefined = undefined;
+  #interval: number;
+
+  constructor(interval: number){
+    this.#interval = interval;
+    this.#startReapLoop();
+  }
+  add<T>(key: string, val: T){
+    this.#cache.set(key,{
+        createdAt: Date.now(),
+        val: val
+    });
+  }
+
+  get<T>(key: string){
+    return this.#cache.get(key)?.val; 
+  }
+
+  #reap(){
+    this.#cache.forEach((entry,key)=>{
+        if(entry.createdAt < Date.now() - this.#interval){
+            this.#cache.delete(key);
+        }
+    })
+  }
+
+  #startReapLoop(){
+    this.#reapIntervalId = setInterval(() => this.#reap(),this.#interval);
+  }
+
+  stopReapLoop(){
+    clearInterval(this.#reapIntervalId);
+    this.#reapIntervalId = undefined;
+  }
+}
